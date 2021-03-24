53c1c0bb130918a311cd6018fd362dad56f30f56
gorsargs <gorsargsyanm@gmail.com>
2021-03-24 15:10:02 +0400

    global variables are deleted

--- index.js
+++ index.js
@@ -4,30 +4,26 @@ import { View, ContextView } from "./src/view.js"
 import InputHandler from "./src/input_handler.js"
 
 const game = new Game();
-const view = new View(320,680,20,10,'gameScreen');
-const view2 = new ContextView(250,680,30,10,'gameScreen2');
+const view = new View(320,680,20,10,'gameScreen',game);
+const view2 = new ContextView(250,680,30,10,'gameScreen2',game);
 
-window.view = view
-window.game = game;
 
 
 
 new InputHandler(game,view);
 
-
+// render loop
 window.requestAnimationFrame(gameLoop);
 
-// render loop
+
 function gameLoop() {
     
-
     view.render(game.getState(),game.colorfield,game.activePiece.color);
 
 
-    view2.render();
+    view2.render(game);
     window.requestAnimationFrame(gameLoop);
 
 };
 
-// move the active piece down every second
 
